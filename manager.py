64e52abed8a116f04987de7a2dcf2824d850d02f
fanklin <bai11720@aliyun.com>
2018-06-20 18:11:14 +0800

    Flask_script和数据库迁移集成

--- manager.py
+++ manager.py
@@ -1,7 +1,10 @@
 # -*- coding:utf-8 -*-
+# 项目立项
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 class Config(object):
     """配置参数"""
@@ -15,17 +18,29 @@ class Config(object):
 
 app = Flask(__name__)
 
+# 加载配置参数
+app.config.from_object(Config)
+
+# 创建链接到数据库的对象
+db = SQLAlchemy(app)
+
+# 创建连接到redis数据库的对象
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+# 创建本地脚本管理器对象
+manger = Manager(app)
+
+# 让迁移时候，app与db建立关联
+Migrate(app, db)
+
+# 将数据库迁移的脚本、命令添加到脚本管理器对象
+manger.add_command('db', MigrateCommand)
+
+
 @app.route('/')
 def hello_world():
     redis_store.set('xx', 'yy')
     return 'Hello World!'
 
-# 加载配置参数
-app.config.from_getobject(Config)
-# 创建链接到数据库的对象
-db = SQLAlchemy(app)
-# 创建连接到redis数据库的对象
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
 if __name__ == '__main__':
-    app.run()
+    manger.run()
