c9f0cadce6460c1fdfafce1e054997c75b000b13
fanklin <bai11720@aliyun.com>
2018-06-20 17:33:24 +0800

    配置文件的加载

--- manager.py
+++ manager.py
@@ -1,11 +1,18 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+class Config(object):
+    DEBUG = True
+
 app = Flask(__name__)
-
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
+app.config.from_object(Config)
+
+db = app()
 if __name__ == '__main__':
     app.run()
